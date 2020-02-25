a1abdb547c79a35851214d79ed26911a6134222e
russellmcintire20 <McIntires@McIntires-MBP.lan>
2020-02-25 06:22:37 -0600

    adding correct files

---
+++ src/robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'LGraham',
+    email: 'lgraham@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Kevin Howell',
+    username: 'KHowell67',
+    email: 'khowell67@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Clementine Bauch',
+    username: 'CBauch123',
+    email: 'cbauch123@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Patricia Lebsack',
+    username: 'PLebsack89',
+    email: 'plebsack89@gmail.com'
+  },
+  {
+    id: 5,
+    name: 'Chelsey Dietrich',
+    username: 'cdietrich',
+    email: 'cdietrich@gmail.com'
+  },
+  {
+    id: 6,
+    name: 'Jordan Halser',
+    username: 'JHalser',
+    email: 'jhalser23@gmail.com'
+  },
+  {
+    id: 7,
+    name: 'Jeremy Cortney',
+    username: 'jcortney456',
+    email: 'jcortney456@gmail.com'
+  },
+  {
+    id: 8,
+    name: 'June Bug',
+    username: 'jbug',
+    email: 'jbug@gmail.com'
+  },
+]
