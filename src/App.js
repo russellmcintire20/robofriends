e12ad3514a094f0dad91803471a660cde2a98638
russellmcintire20 <McIntires@McIntires-MBP.lan>
2020-03-01 09:10:09 -0600

    adding json placeholder with state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: '',
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response=> response.json())
+      .then(users => this.setState({robots: users}));
   }
 
   onsearchChange = (event) => {
@@ -21,13 +26,17 @@ class App extends Component {
     const filteredRobots = this.state.robots.filter(robots =>{
       return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
     })
-    return (
-    <div className='tc'>
-      <h1 className='f1'>Robofriends</h1>
-      <SearchBox searchChange={this.onsearchChange}/>
-      <CardList robots={filteredRobots} />
-    </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>
+    } else {
+      return (
+      <div className='tc'>
+        <h1 className='f1'>Robofriends</h1>
+        <SearchBox searchChange={this.onsearchChange}/>
+        <CardList robots={filteredRobots} />
+      </div>
+      );
+    }
   }
 }
 
